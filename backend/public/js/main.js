eda6ba2d379dc32770d883a7693ffc6fa782c171
Jim Lambert <jlambert12013@gmail.com>
2022-07-06 02:07:35 -0500

    Quick  Commit

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -1,19 +1,4 @@
 "use strict";
-fetch("https:localhost:5000/upload", {
-    method: "POST",
-    headers: {
-        "Content-type": "application/json"
-    },
-    body: JSON.stringify({ name: "Capitain Anonymous" })
-})
-    .then(function (res) {
-    if (res.ok) {
-        console.log("HTTP request successful");
-    }
-    else {
-        console.log("HTTP request unsuccessful");
-    }
-    return res;
-})
+fetch("https://reqres.in/api/users")
     .then(function (res) { return res.json(); })
-    .then(function (data) { return console.log(data); })["catch"](function (error) { return console.log(error); });
+    .then(function (data) { return console.log(data); });
--- backend/src/ts/main.ts
+++ backend/src/ts/main.ts
@@ -1,18 +1,3 @@
-fetch("https:localhost:5000/upload", {
-  method: "POST",
-  headers: {
-    "Content-type": "application/json",
-  },
-  body: JSON.stringify({ name: "Capitain Anonymous" }),
-})
-  .then((res) => {
-    if (res.ok) {
-      console.log("HTTP request successful")
-    } else {
-      console.log("HTTP request unsuccessful")
-    }
-    return res
-  })
+fetch("https://reqres.in/api/users")
   .then((res) => res.json())
   .then((data) => console.log(data))
-  .catch((error) => console.log(error))
